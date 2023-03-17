976ebccef88e610bdfc90c75afd7e6468de7e743
FlakeyCheese <89859893+FlakeyCheese@users.noreply.github.com>
2023-03-17 22:25:14 +0000

    Add project files.

---
+++ Graphing/Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿
+namespace Graphing
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1378, 54);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(91, 28);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(1375, 177);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Generation";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(1391, 207);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(16, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "0";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(1378, 100);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(91, 29);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Stop";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(1394, 416);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(73, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "speed m/s";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Increment = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Location = new System.Drawing.Point(1394, 448);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            5000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(75, 22);
+            this.numericUpDown1.TabIndex = 5;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(1378, 515);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(94, 28);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Colour";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(1378, 566);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(94, 33);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Rainbow";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(1378, 12);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(89, 23);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "Clear";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1481, 753);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Life";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ Graphing/Form1.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Graphing
+{
+    public partial class Form1 : Form
+    {
+        static int width = 100;
+        static int height = 60;
+        Button[,] grid = new Button[width, height];
+        System.Drawing.Color gridColour = Color.Blue;
+        bool rainbow = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int x = 0;
+            int y = 0;
+            
+            for(int i=0;i<width;i=i+1)
+            {
+                for (int j=0;j<height;j=j+1)
+                {
+                    grid[i, j] = new Button
+                    {
+                        Text = "",
+                        Size = new Size(10, 10),
+                        Location = new Point(y, x),
+                        FlatStyle = FlatStyle.Flat,
+                        BackColor = Color.White,
+                         ForeColor = Color.LightGray
+                    };
+                    (grid[i, j]).Click += new EventHandler(anyButton_Click);
+                    this.Controls.Add(grid[i,j]);
+                    
+                    x += 10;
+                }
+                y += 10;
+                x = 0;
+            }
+             
+        }
+
+        private void button1_Click(object sender, EventArgs e)//start button
+        {
+            timer1.Enabled = true; 
+        }
+
+        void anyButton_Click(object sender, EventArgs e) //handlers for all the cell buttons
+        {
+            var button = (sender as Button);
+            if (button.BackColor == gridColour)
+            { button.BackColor = Color.White; }
+            else
+            { button.BackColor = gridColour; }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)//timer 
+        {
+            int count = Int32.Parse(label2.Text); 
+            count++;
+            label2.Text = count.ToString();
+            update(); // call method to calculate new positions
+        }
+        private void update()
+        {
+            int[,] intGrid = new int[width, height];//count the neighbours and store them in intGrid
+            for (int i = 0; i < width; i = i + 1)
+            {
+                for (int j = 0; j < height; j = j + 1)
+                {
+                    int countNeighbours = 0;
+                    int xl=i-1;
+                    if (xl == -1) { xl = width-1; }
+                    int xr = i + 1;
+                    if (xr == width) { xr = 0; }
+                    int yd = j - 1;
+                    if (yd == -1) { yd = height-1; }
+                    int yu = j + 1; 
+                    if (yu == height) { yu = 0; }
+                    // count the neighbours
+                    if (grid[xl, yu].BackColor!= Color.White) { countNeighbours++; }
+                    if (grid[xl, j].BackColor != Color.White) { countNeighbours++; }
+                    if (grid[xl, yd].BackColor != Color.White) { countNeighbours++; }
+                    if (grid[i, yu].BackColor != Color.White) { countNeighbours++; }
+                    if (grid[i, yd].BackColor != Color.White) { countNeighbours++; }
+                    if (grid[xr, yu].BackColor != Color.White) { countNeighbours++; }
+                    if (grid[xr, j].BackColor != Color.White) { countNeighbours++; }
+                    if (grid[xr, yd].BackColor != Color.White) { countNeighbours++; }
+                    intGrid[i, j] = countNeighbours;
+                }
+            }
+            for (int i = 0; i < width; i = i + 1) //check the rules for each cell
+            {
+                for (int j = 0; j < height; j = j + 1)
+                {
+                    if (rainbow)
+                    {
+                        Random rnd = new Random();
+                        Color randomColor = Color.FromArgb(rnd.Next(255), rnd.Next(255), rnd.Next(255));
+
+                        gridColour = randomColor;
+                    }
+                    //1. Any live cell with fewer than two live neighbours dies, as if by underpopulation.
+                    //2. Any live cell with two or three live neighbours lives on to the next generation.
+                    //3. Any live cell with more than three live neighbours dies, as if by overpopulation.
+                    //4. Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+                    if (intGrid[i, j] < 2 && (grid[i, j].BackColor != Color.White)) { grid[i, j].BackColor = Color.White; }// rule 1
+                    if (intGrid[i, j] == 3 && (grid[i, j].BackColor == Color.White)) { grid[i, j].BackColor = gridColour; }// rule 4
+                    if (intGrid[i, j] > 3 && (grid[i, j].BackColor != Color.White)) { grid[i, j].BackColor = Color.White; }//rule 3
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e) //stop button
+        {
+            timer1.Enabled = false;
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e) //adjust speed
+        {
+            timer1.Interval = (int)(numericUpDown1.Value);
+        }
+
+        private void button3_Click(object sender, EventArgs e)// choose colour
+        {
+            colorDialog1.ShowDialog();
+            gridColour = colorDialog1.Color;
+            for (int i = 0; i < width; i = i + 1)
+            {
+                for (int j = 0; j < height; j = j + 1)
+                {
+                    if (grid[i,j].BackColor != Color.White)
+                    { grid[i, j].BackColor = gridColour; }
+                }
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)//random colour
+        {
+            rainbow = !rainbow;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+            for (int i = 0; i < width; i = i + 1)
+            {
+                for (int j = 0; j < height; j = j + 1)
+                {
+                    grid[i, j].BackColor = Color.White;
+                    
+                }
+            }
+        }
+    }
+}
